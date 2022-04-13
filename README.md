a29f86c60072788c78ff0f3c1889b72f7fade62b
ARXChrono <chronoxp@hotmail.com>
2022-04-13 17:16:40 +0800

    project init

---
+++ README.md
@@ -0,0 +1,13 @@
+# 🌎 Berlitz Marketing Test
+
+My 2022 take on berlitz's marketing test.
+
+## Netlify Preview
+
+TODO
+
+## Bootup Development Server
+
+```shell
+  npm run develop
+```
---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: `react-test`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-react-helmet"]
+};
